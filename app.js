8814d33e0030f12d189644b56c9fb99fec161c3b
Mizanur Rahman <mizanur.rahman@navana.com>
2022-06-11 20:58:37 +0600

    include exclude ts config file tsc --init

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
+"use strict";
 /** @format */
 function add(n1, n2) {
     return n1 + n2;
 }
-var number1 = '5';
-var number2 = 2.2;
-var result = add(number1, number2);
+const number1 = 5;
+const number2 = 2.2;
+const number3 = 2.2;
+const result = add(number1, number2);
 console.log(result);
--- app.ts
+++ app.ts
@@ -6,6 +6,7 @@ function add(n1: number, n2: number) {
 
 const number1 = 5;
 const number2 = 2.2;
+const number3 = 2.2;
 
 const result = add(number1, number2);
 
